385da94a4775833de51b5c9874711ed7e71626da
eghafoo <c28eric.ghafoor@afacademy.af.edu>
2026-02-12 09:44:28 -0700

    checkpoint

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,6 +49,14 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
+       w_addends <= x"0F"; w_Cin <= '1'; wait for 10 ns;
+           assert (w_sum = x"0" and w_Cout = '1') report "bad carry ripple" severity failure;
+       w_addends <= x"35"; w_Cin <= '0'; wait for 10 ns;
+           assert (w_sum = x"8" and w_Cout = '0') report "bad rando nums" severity failure;
+       w_addends <= x"69"; w_Cin <= '0'; wait for 10 ns;
+           assert (w_sum = x"F" and w_Cout = '0') report "bad rando nums" severity failure;
+       w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+           assert (w_sum = x"1" and w_Cout = '0') report "bad Cin only" severity failure;    
 	
 		wait; -- wait forever
 	end process;	
